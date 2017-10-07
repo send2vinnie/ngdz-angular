d82e5240a52e80e5a86c6f67c086b5d5b7a3684f
badre429@hotmail.com <badre429@hotmail.com>
2017-10-07 14:02:01 +0100

    feat add global route var

--- src/app-core/app-routing.module.ts
+++ src/app-core/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { appRoutes } from '../modules/route';
 
-const routes: Routes = [];
+const routes: Routes = [
+  ... appRoutes
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
