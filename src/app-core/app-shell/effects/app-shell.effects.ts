a89c4d14adcf76c657a1344f5c62ab88118ce5ab
badre429@hotmail.com <badre429@hotmail.com>
2017-10-20 20:58:57 +0100

    chore load application config

--- src/app-core/app-shell/effects/app-shell.effects.ts
+++ src/app-core/app-shell/effects/app-shell.effects.ts
@@ -9,20 +9,24 @@ import { Effect, Actions } from '@ngrx/effects';
 import { of } from 'rxjs/observable/of';
 
 import * as Auth from '../../app-auth/actions/auth';
+import { HttpClient } from '@angular/common/http';
+import { AppConfig } from '../../../modules/app-config';
 
 @Injectable()
 export class AppShellEffects {
 
-    @Effect({ dispatch: false })
-    loginSuccessNavigation$ = this.actions$
-        .ofType(Auth.LOGIN_SUCCESS)
-        .do(() => console.log('application is loged in loading application inforation '));
+  @Effect({ dispatch: false })
+  loadApplicationConfig$ = this.actions$
+    .ofType(Auth.LOGIN_SUCCESS, Auth.NOT_LOGGED_IN, Auth.LOGOUT)
+    .do(() => console.log('application is loged in loading application inforation '));
 
 
-    constructor(
-        private actions$: Actions,
-        private router: Router
-    ) {
+  constructor(
+    private actions$: Actions,
+    private http: HttpClient,
+    private router: Router,
+    private config: AppConfig
+  ) {
 
-    }
+  }
 }
