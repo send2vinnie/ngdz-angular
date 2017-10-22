48ee1681e847651cf88b3eb8a5a244f8f4b5e7e1
badre429 <badre429@gmail.com>
2017-10-22 13:45:52 +0100

    feat(load main shell) only if application is ready

--- src/app-core/app-shell/containers/main-shell/main-shell.component.ts
+++ src/app-core/app-shell/containers/main-shell/main-shell.component.ts
@@ -3,7 +3,8 @@ import { Observable } from 'rxjs/Observable';
 import { Component, ChangeDetectionStrategy, OnInit, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 
-import * as fromRoot from '../../reducers';
+import * as fromLayout from '../../reducers';
+import * as fromApp from '../../../reducers/app-reducers';
 import * as fromAuth from '../../../app-auth/reducers';
 
 import * as Auth from '../../../app-auth/actions/auth';
@@ -24,20 +25,21 @@ export class MainShellComponent implements OnInit, OnDestroy {
 
 
   Subscriptions: Subscription[] = [];
-
+  AppReady$ = this.store.select(fromLayout.getLayout);
   constructor(
     private menuService: MenuService,
     private router: Router,
-    private store: Store<fromRoot.State>) {
+    private store: Store<fromLayout.State>) {
     /**
      * Selectors can be applied with the `select` operator which passes the state
      * tree to the provided selector
      */
+    this.store.dispatch(new Auth.LoadInitialState());
+
 
   }
 
   ngOnInit(): void {
-    this.store.dispatch(new Auth.LoadInitialState());
     this.Subscriptions.push(this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         this.store.dispatch(new SetCurrentlyOpenByRouteAction(event.urlAfterRedirects));
--- src/app-core/reducers/app-reducers.ts
+++ src/app-core/reducers/app-reducers.ts
@@ -1,25 +1,47 @@
 import * as app from '../actions/app-actions';
-import { createFeatureSelector } from '@ngrx/store';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { AbpConfig } from '../abp/models/abp-config';
 
 // tslint:disable-next-line:no-empty-interface
-export interface State {
+export interface State extends AbpConfig {
+  ready: boolean;
 
 }
 
-const initialState: State = {
+const initialState: State = <State>{
+  ready: false
 
 };
 
 export function reducer(state = initialState, action: app.Actions): State {
   switch (action.type) {
     case app.LOAD_APP_CONFIG_SUCCESS:
-      return { ...action.payload };
-
+      return { ...action.payload, ready: true };
+    case app.LOAD_APP_CONFIG:
+      return { ...state, ready: false };
     default:
       return state;
   }
 }
 export const getAppConfig = createFeatureSelector<State>('config');
 
+export const getAppReady =
+  createSelector(getAppConfig, (state: State) => state.ready);
+export const getAuthInformation =
+  createSelector(getAppConfig, (state: State) => state.auth);
+export const getSessionInformation =
+  createSelector(getAppConfig, (state: State) => state.session);
 
+export const getFeatures =
+  createSelector(getAppConfig, (state: State) => state.features);
+
+
+export const getLocalization =
+  createSelector(getAppConfig, (state: State) => state.localization);
+
+export const getTiming =
+  createSelector(getAppConfig, (state: State) => state.timing);
+
+  export const getSetting =
+  createSelector(getAppConfig, (state: State) => state.setting);
 
